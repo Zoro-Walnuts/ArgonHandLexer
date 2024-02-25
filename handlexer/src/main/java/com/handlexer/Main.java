70dfeb1ae9bced83cafd9950359ba03dbdad8a15
Walnuts <zed.walnuts.king@gmail.com>
2024-02-25 18:02:07 +0800

    added jar file for easier execution

--- handlexer/src/main/java/com/handlexer/Main.java
+++ handlexer/src/main/java/com/handlexer/Main.java
@@ -1,14 +1,23 @@
 package com.handlexer;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         HandLexer lex = new HandLexer();
-        if (args.length > 0) {
+        if (args.length >= 0) {
             try {
+                // set file for command argument:
                 File file = new File("handlexer/src/main/resources/" + args[0]);
-                // System.out.println(file.getAbsolutePath());
+
+                // set file for user input prompt:
+                // Scanner scanner = new Scanner(System.in);
+                // String input_file;
+                // System.out.println("Input file name/path: ");
+                // input_file = scanner.nextLine();
+                // File file = new File(input_file);
+
                 lex.Analyze(new File(file.getAbsolutePath()));
             } catch (FileNotFoundException e) {
                 System.out.println("File not found :3");
