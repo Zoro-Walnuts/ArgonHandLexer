04d87b68ec83017b90bbb0576de2ab457f5af79c
Walnuts <zed.walnuts.king@gmail.com>
2024-02-25 20:24:17 +0800

    made execution easier with the jar file

--- handlexer/src/main/java/com/handlexer/Main.java
+++ handlexer/src/main/java/com/handlexer/Main.java
@@ -6,28 +6,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         HandLexer lex = new HandLexer();
-        if (args.length > 0) {
-            try {
-                // set file for command argument:
-                File file = new File("handlexer/src/main/resources/" + args[0]);
-
-                // set file for user input prompt:
-                // Scanner scanner = new Scanner(System.in);
-                // String input_file;
-                // System.out.println("Input file name/path: ");
-                // input_file = scanner.nextLine();
-                // File file = new File(input_file);
-
-                lex.Analyze(new File(file.getAbsolutePath()));
-            } catch (FileNotFoundException e) {
-                System.out.println("File not found :3");
-            } catch (IOException ioe) {
-                System.out.println(ioe.getMessage());
-            } catch (IllegalStateException ise) {
-                System.out.println(ise.getMessage());
+        try {
+            File file;
+            if (args.length > 0) {
+                // check argument for file:
+                file = new File(args[0]);
+            } else {
+                // get file from user input prompt:
+                Scanner scanner = new Scanner(System.in);
+                String input_file;
+                System.out.print("Input file name/path: ");
+                input_file = scanner.nextLine();
+                file = new File(input_file);
+                scanner.close();
             }
-        } else {
-            System.out.println("Invalid File Input!");
+
+            lex.Analyze(new File(file.getAbsolutePath()));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found :3");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        } catch (IllegalStateException ise) {
+            System.out.println(ise.getMessage());
         }
     }
 }
